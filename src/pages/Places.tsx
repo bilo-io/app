cf6cb02c9031d1a31623d05a5f6315bace9de1ec
Bilo Lwabona <bilo.lwabona@gmail.com>
2024-08-30 12:35:45 +0200

    chore: added input to map

--- src/pages/Places.tsx
+++ src/pages/Places.tsx
@@ -1,4 +1,4 @@
-import { Accordion, Button, Form, Map, Tab, Table, Tabs, Typography, mapToOptions, useRenderField } from 'components/Core';
+import { Accordion, Button, Form, Input, Map, Tab, Table, Tabs, Typography, mapToOptions, useRenderField } from 'components/Core';
 import { ControlsHeader } from 'components/Domain';
 import React, { useMemo, useState } from 'react';
 import { useAppTheme, useTranslations } from 'hooks';
@@ -28,9 +28,19 @@ export const ApiEntertainment: React.FC = () => {
         },
         {
             id: 1,
+            key: 'places',
+            label: (
+                <ControlsHeader icon="location" label={'Places'} />
+            ),
+            view: (
+                <PlacesContainer />
+            )
+        },
+        {
+            id: 2,
             key: 'assignment',
             label: (
-                <ControlsHeader icon="map" label={'Assignment'} />
+                <ControlsHeader icon="info-circle" label={'Assignment'} />
             ),
             view: (
                 <div className='p-4'>
@@ -38,17 +48,6 @@ export const ApiEntertainment: React.FC = () => {
                 </div>
             ),
         },
-        {
-            id: 2,
-            key: 'places',
-            label: (
-                <ControlsHeader icon="location" label={'Places'} />
-            ),
-            view: (
-                <ActorsContainer />
-            )
-        },
-
     ]), []);
 
     return (
@@ -287,7 +286,7 @@ export const ActorsContainer: React.FC = () => {
 
 export const MapsContainer: React.FC = () => {
     const { t } = useTranslations();
-    const { name: themeName } = useAppTheme();
+    const { theme, name: themeName } = useAppTheme();
     const { renderField } = useRenderField();
     const { loading, data, error, fetchData } = useFetchApi<IPaginated<IPlace>>({
         method: 'GET',
@@ -304,16 +303,43 @@ export const MapsContainer: React.FC = () => {
         fetchData({ method: 'GET', url: `${API_BASE_URL}/movies?${toUrlParams(query)}` });
     };
 
+    const handleSearch = (e: any) => {
+        setQuery((prev: any) => ({
+            ...prev,
+            search: e
+        }));
+    };
+
     const mapPositions = data?.data.map((place: IPlace) => place.coordinates);
     console.log({ mapPositions });
 
     return (
         <div className="w-full h-full">
-            <Map
-                // @ts-ignore
-                theme={themeName as string}
-                positions={mapPositions as any[]}
-            />
+            <div className="z-0">
+                <Map
+                    // @ts-ignore
+                    theme={themeName as string}
+                    positions={mapPositions as any[]}
+                />
+            </div>
+            <div className="absolute w-3/5 md:w-80 mt-0 top-4 z-100 px-2" style={{
+                zIndex: 1000,
+                left: 'calc(50% - 160px)',
+                backgroundColor: theme?.BACKGROUND,
+                borderRadius: '4rem',
+                overflow: 'hidden'
+            }}>
+                <Input
+                    value={query.search}
+                    onChange={handleSearch}
+                    className="w-full my-0 px-2 border-none"
+                    placeholder='Search places'
+                    style={{
+                        marginBottom: 0,
+                        marginTop: 0,
+                    }}
+                />
+            </div>
         </div>
     );
 };
@@ -337,5 +363,95 @@ export interface ICompany {
     logoUrl: string,
     description: string,
 }
+
+export const PlacesContainer: React.FC = () => {
+    const { renderField } = useRenderField();
+    const { loading, data, error, fetchData } = useFetchApi<IPaginated<IPlace>>({
+        method: 'GET',
+        url: `${API_BASE_URL}/places?limit=100`,
+    });
+
+    const [view, setView] = useState<'table' | 'grid' | 'list'>('table');
+    const [query, setQuery] = useState<any>({
+        dateOfBirth: undefined,
+        search: '',
+    });
+
+    const fetchApiData = (): void => {
+        fetchData({ method: 'GET', url: `${API_BASE_URL}/movies?${toUrlParams(query)}` });
+    };
+
+    const columns = useMemo(() => ([
+        {
+            Cell: ({ value, row }: any) => (
+                <div>
+                    <img
+                        src={value}
+                        className="h-12" alt={`place:${row.original?.name}`}
+                    />
+                </div>
+            ),
+            Header: '',
+            accessor: 'img'
+        },
+        {
+            Header: 'Name',
+            accessor: 'name'
+        },
+        {
+            Cell: ({ value }: any) => (
+                <div>
+                    {value}
+                </div>
+            ),
+            Header: 'Birthday',
+            accessor: 'dateOfBirth'
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    ]), [data]);
+
+    if (loading) return <div>Loading...</div>;
+    if (!_.isEmpty(error)) return <div>Error...{JSON.stringify(error, null, 2)}</div>;
+
+    return (
+        <div className="page-content">
+            <Form
+                initialValues={query}
+                onSubmit={fetchApiData}
+                onChange={(t) => console.log(t)}
+            >
+                {({ values, handleChange, handleSubmit }) => (
+                    <>
+                        <div className="p-2">
+                            {renderField('search', 'text')}
+                            {renderField('dateOfBirth', 'text')}
+                        </div>
+
+                        <Button onClick={() => handleSubmit()}>
+                            Search
+                        </Button>
+                        <br />
+                        <br />
+                    </>
+                )}
+            </Form>
+
+            {view === 'table' && (
+                <Table
+                    columns={columns}
+                    data={(data?.data as IPlace[]) || []}
+                />
+            )}
+
+            {view === 'grid' && (
+                <div>Grid</div>
+            )}
+
+            {view === 'list' && (
+                <div>List</div>
+            )}
+        </div>
+    );
+};
 
 export default ApiEntertainment;
--- src/pages/assignments.ts
+++ src/pages/assignments.ts
@@ -2,61 +2,18 @@ export const assignments = {
     entertainment: `
 # Assessment: Entertainment
 
-- **Level**: Intermediate
+## **Level**: Intermediate
 
 ### Task Summary
 
 This task is to create a web interface that pulls data from an API and displays it on a map.
 Furthermore, the user should use a form to filter the type of data being shown on the map.
 
-### API
-
-#### Endpoints
-
-There are several API endpoints, and there is a [postman collection]() to simplify using these.
-
-| Endpoint              | Description                                        |
-| :-------------------- | :------------------------------------------------- |
-| GET \`/api/places/ \`    | Fetches a list of \`places\`, along with coordinates |
-| GET \`/api/places/: id\` | Fetches a single \`place\` by ID                     |
-
-#### Sorting, Paging & Filtering
-
-The "list" endpoints do support paging and filtering, both of which retrieve subsets of the data.
-
-Examples:
-
-- Sorting:
-  - \`/ api/places ? sortBy = name & sortDirection=desc\` would sort by the \`name\` field in \`desc\` (descending) order.
-  - > Note: \`sorting\` is done before \`paging\` or \`filtering\` for more commonly expected behaviour.
-    >
-- Filtering:
-  - \`/api/places?search=Don&filter[category]=food\`
-  - > This fetches all places which have the name \`"Don"\` and the category \`"food"\`
-- Paging:
-  - \`/api/places?page=2&limit=3\`
-  - > This fetches up to a \`limit\` of \`3\` places, and if there are more, then it goes to the second set of 3 results (i.e. \`page\` being \`2\`)
-
-### Requirements
-
-- Implement the proposed design as closely as possible
-- Create a webapplication using React and Typescript
-- What you use for styling is entirely up to you
-  - > (we use StyledComponents, but I'm happy with \`Sass\`, \`Less\`, \`Tailwind\` etc.)
-    >
-- The data should be fetched asynchronously (using a library of choice, or simply the built-in \`fetch\` is enough)
-- The UI form should allow for filtering of the data
-- The whole application should be hosted on [Vercel](https://vercel.app) and the source code should be shared via [Github](https://github.com/) for us to review
-
-### Design
-
-![map-interface]()
-
     `,
     places: `
 # Assessment: Maps & Places
 
-- **Level**: Intermediate
+## **Level**: Intermediate
 
 ## Task Summary
 
@@ -67,55 +24,61 @@ Furthermore, the user should use a form to filter the type of data being shown o
 
 ### Requirements
 
-- Implement the proposed design as closely as possible
-- Create a webapplication using React and Typescript
-- What you use for styling is entirely up to you
+- 1. Implement the proposed design as closely as possible
+- 2. Create a webapplication using React and Typescript
+- 3. What you use for styling is entirely up to you
   - > (we use StyledComponents, but I'm happy with \`Sass\`, \`Less\`, \`Tailwind\` etc.)
     >
-- The data should be fetched asynchronously (using a library of choice, or simply the built-in \`fetch\` is enough)
-- The UI form should allow for filtering of the data
-- The whole application should be hosted on [Vercel](https://vercel.app) and the source code should be shared via [Github](https://github.com/) for us to review
+- 4. The data should be fetched asynchronously (using a library of choice, or simply the built-in \`fetch\` is enough)
+- 5. The UI form should allow for filtering of the data
+- 6. The whole application should be hosted on [Vercel](https://vercel.app) and the source code should be shared via [Github](https://github.com/) for us to review
 
 ![screenshot]()
 
+---
 
 ## API
 
+We have provided a mock API to make the task a bit simpler.
+
+API_BASE_URL: [https://api-octiv-test.vercel.app/](https://api-octiv-test.vercel.app/)
+
 ### Endpoints
 
-> There are several API endpoints, and there is a [postman collection]() to simplify using these.
+You will be interested in the following endpoints:
 
 <br />
 
-| Endpoint              | ...   Description                                        |
+---
+
+| Endpoint              | Description                                        |
 | :-------------------- | :------------------------------------------------- |
-| **GET** \`/api/places/\`    | -> Fetches a list of \`places\`, along with coordinates |
-| **GET** \`/api/places/:id\` | -> Fetches a single \`place\` by ID                     |
+| GET \`/api/places/ \`    | Fetches a list of \`places\`, along with coordinates |
+| GET \`/api/places/: id\` | Fetches a single \`place\` by ID                     |
+---
 
 ### Sorting, Paging & Filtering
 
 The "list" endpoints do support paging and filtering, both of which retrieve subsets of the data.
 
-### Examples
+### Examples:
 
-- **Sorting**:
-  - **GET** \`/api/places?sortBy=name&sortDirection=desc\` would sort by the \`name\` field in \`desc\` (descending) order.
-  - > Note: \`sorting\` is done before \`paging\` or \`filtering\` for more commonly expected behaviour.
-
-<br />
-
-- **Filtering**:
-  - **GET** \`/api/places?search=Don&filter[category]=food\`
-  - > This fetches all places which have the name \`"Don"\` and the category \`"food"\`
+**Sorting**:
+- **GET** \`/api/places?sortBy=name&sortDirection=desc\` would sort by the \`name\` field in \`desc\` (descending) order.
+- > Note: \`sorting\` is done before \`paging\` or \`filtering\` for more commonly expected behaviour.
 
 <br />
 
-- **Paging**:
-  - **GET** \`/ api/places?page=2&limit=3\`
-  - > This fetches up to a \`limit\` of \`3\` places, and if there are more, then it goes to the second set of 3 results (i.e. \`page\` being \`2\`)
-    >
+**Filtering**:
+- **GET** \`/api/places?search=Don&filter[category]=food\`
+- > This fetches all places which have the name \`"Don"\` and the category \`"food"\`
 
+<br />
 
+**Paging**:
+- **GET** \`/ api/places?page=2&limit=3\`
+- > This fetches up to a \`limit\` of \`3\` places, and if there are more, then it goes to the second set of 3 results (i.e. \`page\` being \`2\`)
+  >
     `,
     schedule: `
     `
